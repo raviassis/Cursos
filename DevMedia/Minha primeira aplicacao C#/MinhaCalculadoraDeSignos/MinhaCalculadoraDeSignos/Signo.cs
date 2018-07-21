0df8988f0cc80c1372e05712a7f75b260b087a45
raviassis <ravi.g.assis@gmail.com>
2018-07-21 03:58:38 -0300

    up

---
+++ DevMedia/Minha primeira aplicacao C#/MinhaCalculadoraDeSignos/MinhaCalculadoraDeSignos/Signo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaCalculadoraDeSignos
+{
+    class Signo
+    {
+        private string nome;
+        private int diaInicio;
+        private int mesInicio;
+        private int diaFim;
+        private int mesFim;
+        private string caracteristicas;
+    }
+}
