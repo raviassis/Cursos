16cfaca53b8169e15cb7dc5785f913a967b685ef
raviassis <ravi.g.assis@gmail.com>
2018-07-23 02:26:24 -0300

    Serializer

---
+++ DevMedia/Introducao ao .NET Framework/Framework/System.XML_Exemple/Contato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace System.XML_Exemple
+{
+    [Serializable()]
+    public class Contato
+    {
+        [XmlElement("Nome")]
+        public string Nome;
+        [XmlElement("Telefone")]
+        public string Telefone;
+    }
+}
