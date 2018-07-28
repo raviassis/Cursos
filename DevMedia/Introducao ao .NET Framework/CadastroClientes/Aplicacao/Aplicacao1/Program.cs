2c8f2f21873b33d32e2dc3c48c42bb61946131cc
raviassis <ravi.g.assis@gmail.com>
2018-07-28 03:30:54 -0300

    Comandos e StoreProcedures

---
+++ DevMedia/Introducao ao .NET Framework/CadastroClientes/Aplicacao/Aplicacao1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Aplicacao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe Nome do Cliente: ");
+            var NomeCliente = Console.ReadLine();
+            Console.WriteLine("Informe Email do Cliente: ");
+            var Email = Console.ReadLine();
+
+            var ConStr = ConfigurationManager.ConnectionStrings["Cadastro"].ConnectionString;
+            var con = new SqlConnection(ConStr);
+            //var sql = "insert into Clientes(NomeCliente, EmailCliente) values (@NomeCliente, @EmailCliente);";
+
+            var sql = "InsertCliente";
+            var cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@NomeCliente", NomeCliente);
+            cmd.Parameters.AddWithValue("@EmailCliente", Email);
+
+            con.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Registro inserido com sucesso!");
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+    }
+}
