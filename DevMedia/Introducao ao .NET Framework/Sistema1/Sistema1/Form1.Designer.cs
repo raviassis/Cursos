3ca196999d62d7e8c94393cc156da70e73758202
raviassis <ravi.g.assis@gmail.com>
2018-07-31 03:44:12 -0300

    POO Abstract

---
+++ DevMedia/Introducao ao .NET Framework/Sistema1/Sistema1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Sistema1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Location = new System.Drawing.Point(67, 41);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(159, 20);
+            this.txtMensagem.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Aviso";
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Items.AddRange(new object[] {
+            "EMAIL",
+            "SMS",
+            "WHATSAPP"});
+            this.cmbTipo.Location = new System.Drawing.Point(31, 79);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(121, 21);
+            this.cmbTipo.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Avisar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMensagem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DevMedia/Introducao ao .NET Framework/Sistema1/Sistema1/Framework.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema1
+{
+    public enum TipoEnvio { EMAIL = 0, SMS = 1, WHATSAPP = 2}
+
+    public abstract class FormaEnvio
+    {
+        public abstract void Enviar(string aviso);
+    }
+
+    public class Email : FormaEnvio
+    {
+        public override void Enviar(string aviso) {
+            MessageBox.Show("Email enviado: " + aviso);
+        }
+    }
+
+    public class SMS : FormaEnvio
+    {
+        public override void Enviar(string aviso)
+        {
+            MessageBox.Show("SMS enviado: " + aviso);
+        }
+    }
+
+    public class WhatsApp : FormaEnvio
+    {
+        public override void Enviar(string aviso)
+        {
+            MessageBox.Show("WhatsApp enviado: " + aviso);
+        }
+    }
+
+    public class Fabrica
+    {
+        public static FormaEnvio CriarEnvio(TipoEnvio tipo)
+        {
+            switch (tipo)
+            {
+                case TipoEnvio.SMS:
+                    return new SMS();
+                    break;
+                case TipoEnvio.EMAIL:
+                    return new Email();
+                    break;
+                case TipoEnvio.WHATSAPP:
+                    return new WhatsApp();
+                    break;
+                default:
+                    return null;
+                    break;
+            }
+        }
+    }
+}
